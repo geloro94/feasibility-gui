a05e913b47772d1a73a611a01fc7241408b316e6
shah1375 <shayanmohajerani@gmail.com>
2023-10-15 16:23:00 +0200

    moved import file functions to component

--- src/app/modules/querybuilder/components/querybuilder-overview/import-query/import-query.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-overview/import-query/import-query.component.ts
@@ -1,8 +1,56 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, ChangeDetectorRef, Component } from '@angular/core';
+import { Query } from '../../../model/api/query/query';
+import { QueryProviderService } from '../../../service/query-provider.service';
+import { Router } from '@angular/router';
+import { FeatureProviderService } from '../../../service/feature-provider.service';
+import { ApiTranslator } from '../../../controller/ApiTranslator';
 
 @Component({
   selector: 'num-import-query',
   templateUrl: './import-query.component.html',
   styleUrls: ['./import-query.component.scss'],
 })
-export class ImportQueryComponent {}
+export class ImportQueryComponent implements AfterViewChecked {
+  fileName: string;
+  importQuery: Query;
+  query: Query;
+  actionDisabled: boolean;
+
+  constructor(
+    public queryProviderService: QueryProviderService,
+    private router: Router,
+    public featureProviderService: FeatureProviderService,
+    private apiTranslator: ApiTranslator,
+    private changeDetector: ChangeDetectorRef
+  ) {}
+
+  ngAfterViewChecked(): void {
+    if (this.importQuery) {
+      this.actionDisabled = false;
+    } else {
+      this.actionDisabled = true;
+    }
+    this.changeDetector.detectChanges();
+  }
+
+  doImportFromFile(event: Event): void {
+    const file: File = (event.target as HTMLInputElement).files[0];
+    const reader = new FileReader();
+    reader.onload = this.onReaderLoad.bind(this);
+    reader.readAsText(file);
+    this.fileName = file.name;
+  }
+
+  onReaderLoad(event): void {
+    this.importQuery = JSON.parse(event.target.result);
+  }
+
+  doImport(): void {
+    this.query = this.apiTranslator.translateImportedSQtoUIQuery(
+      QueryProviderService.createDefaultQuery(),
+      this.importQuery
+    );
+    this.queryProviderService.store(this.query);
+    this.router.navigate(['/querybuilder/editor'], { state: { preventReset: true } });
+  }
+}
--- src/app/modules/querybuilder/components/querybuilder-overview/querybuilder-overview.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-overview/querybuilder-overview.component.ts
@@ -15,28 +15,10 @@ import { IAppConfig } from '../../../../config/app-config.model';
   templateUrl: './querybuilder-overview.component.html',
   styleUrls: ['./querybuilder-overview.component.scss'],
 })
-export class QuerybuilderOverviewComponent implements OnInit, AfterViewChecked {
-  private features: IAppConfig;
-  queryVersion: string;
-  importQuery: Query;
-  actionDisabled: boolean;
-
-  constructor(
-    public queryProviderService: QueryProviderService,
-    private httpClient: HttpClient,
-    private router: Router,
-    private backend: BackendService,
-    private feature: FeatureService,
-    public featureProviderService: FeatureProviderService,
-    private changeDetector: ChangeDetectorRef,
-    private apiTranslator: ApiTranslator
-  ) {}
+export class QuerybuilderOverviewComponent implements OnInit {
+  constructor(public queryProviderService: QueryProviderService, private backend: BackendService) {}
 
   private savedTemplatesSubscription: Subscription;
-
-  query: Query;
-  title = '';
-  comment = '';
 
   savedTemplates: Array<{
     id?: number
@@ -48,43 +30,7 @@ export class QuerybuilderOverviewComponent implements OnInit, AfterViewChecked {
     isValid?: boolean
   }> = [];
 
-  fileName: string;
-
-  ngOnInit(): void {
-    this.query = this.queryProviderService.query();
-    this.features = this.featureProviderService.getFeatures();
-    this.queryVersion = this.features.queryVersion;
-  }
-
-  ngAfterViewChecked(): void {
-    if (this.importQuery) {
-      this.actionDisabled = false;
-    } else {
-      this.actionDisabled = true;
-    }
-    this.changeDetector.detectChanges();
-  }
-
-  doImportFromFile(event: Event): void {
-    const file: File = (event.target as HTMLInputElement).files[0];
-    const reader = new FileReader();
-    reader.onload = this.onReaderLoad.bind(this);
-    reader.readAsText(file);
-    this.fileName = file.name;
-  }
-
-  onReaderLoad(event): void {
-    this.importQuery = JSON.parse(event.target.result);
-  }
-
-  doImport(): void {
-    this.query = this.apiTranslator.translateImportedSQtoUIQuery(
-      QueryProviderService.createDefaultQuery(),
-      this.importQuery
-    );
-    this.queryProviderService.store(this.query);
-    this.router.navigate(['/querybuilder/editor'], { state: { preventReset: true } });
-  }
+  ngOnInit(): void {}
 
   doValidate(): void {
     this.savedTemplatesSubscription = this.backend.loadSavedTemplates(true).subscribe((queries) => {
