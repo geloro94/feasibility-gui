955dd35e637ceb66fa044acd90c7e2c02563d5d9
shah1375 <shayanmohajerani@gmail.com>
2023-10-15 16:02:44 +0200

    new component import-query

---
+++ src/app/modules/querybuilder/components/querybuilder-overview/import-query/import-query.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ImportQueryComponent } from './import-query.component';
+
+describe('ImportQueryComponent', () => {
+  let component: ImportQueryComponent;
+  let fixture: ComponentFixture<ImportQueryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ImportQueryComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ImportQueryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/querybuilder/components/querybuilder-overview/import-query/import-query.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'num-import-query',
+  templateUrl: './import-query.component.html',
+  styleUrls: ['./import-query.component.scss'],
+})
+export class ImportQueryComponent {}
