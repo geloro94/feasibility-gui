4f8f8cce4c2de04eb0fa29a6d0e57f945591f9a0
shah1375 <shayanmohajerani@gmail.com>
2023-10-18 23:01:19 +0200

    added functions for closing dialog and passing query

--- src/app/modules/querybuilder/components/querybuilder-editor/save/save-dialog/download-query/download-query.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-editor/save/save-dialog/download-query/download-query.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FileSaverService } from 'ngx-filesaver';
 import { ApiTranslator } from 'src/app/modules/querybuilder/controller/ApiTranslator';
 import { Query } from 'src/app/modules/querybuilder/model/api/query/query';
+import { SaveDialogComponent } from '../save-dialog.component';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'num-download-query',
@@ -11,13 +13,20 @@ import { Query } from 'src/app/modules/querybuilder/model/api/query/query';
 export class DownloadQueryComponent {
   downloadQuery: boolean;
   filename = '';
+
+  @Input()
   query: Query;
 
-  constructor(private fileSaverService: FileSaverService, private apiTranslator: ApiTranslator) {}
+  constructor(
+    private fileSaverService: FileSaverService,
+    private apiTranslator: ApiTranslator,
+    private dialogRef: MatDialogRef<SaveDialogComponent, void>
+  ) {}
 
   doDownloadQuery(): void {
     const queryString = JSON.stringify(this.apiTranslator.translateToV2(this.query));
     const fileData = new Blob([queryString], { type: 'text/plain;charset=utf-8' });
     this.fileSaverService.save(fileData, this.filename + '.json');
+    this.dialogRef.close();
   }
 }
--- src/app/modules/querybuilder/components/querybuilder-editor/save/save-dialog/resultant-query/resultant-query.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-editor/save/save-dialog/resultant-query/resultant-query.component.ts
@@ -3,6 +3,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
 import { Query } from 'src/app/modules/querybuilder/model/api/query/query';
 import { BackendService } from 'src/app/modules/querybuilder/service/backend.service';
+import { SaveDialogComponent } from '../save-dialog.component';
 
 @Component({
   selector: 'num-resultant-query',
@@ -21,7 +22,10 @@ export class ResultantQueryComponent implements OnInit, OnDestroy {
 
   saveButtonDisabled = true;
 
-  constructor(public backend: BackendService) {}
+  constructor(
+    public backend: BackendService,
+    private dialogRef: MatDialogRef<SaveDialogComponent, void>
+  ) {}
 
   private subscriptionResult: Subscription;
 
@@ -43,6 +47,8 @@ export class ResultantQueryComponent implements OnInit, OnDestroy {
     this.subscriptionResult?.unsubscribe();
     this.subscriptionResult = this.backend
       .saveQuery(this.query, this.title, this.comment, true)
-      .subscribe((response) => {});
+      .subscribe(() => {
+        this.dialogRef.close();
+      });
   }
 }
