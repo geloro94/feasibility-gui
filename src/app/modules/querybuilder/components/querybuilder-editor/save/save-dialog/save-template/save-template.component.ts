ef94cdd3a611ac21ab0ae7b3bc1c17841bcf6626
shah1375 <shayanmohajerani@gmail.com>
2023-10-18 16:56:55 +0200

    function for saving templates

--- src/app/modules/querybuilder/components/querybuilder-editor/save/save-dialog/save-template/save-template.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-editor/save/save-dialog/save-template/save-template.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Query } from 'src/app/modules/querybuilder/model/api/query/query';
+import { BackendService } from 'src/app/modules/querybuilder/service/backend.service';
 
 @Component({
   selector: 'num-save-template',
   templateUrl: './save-template.component.html',
   styleUrls: ['./save-template.component.scss'],
 })
-export class SaveTemplateComponent {
+export class SaveTemplateComponent implements OnDestroy {
   title = '';
 
   comment = '';
+
+  query: Query;
+
+  constructor(public backend: BackendService) {}
+
+  private subscriptionResult: Subscription;
+
+  ngOnDestroy(): void {
+    this.subscriptionResult?.unsubscribe();
+  }
+
+  doSaveTemplate(): void {
+    this.subscriptionResult?.unsubscribe();
+    this.subscriptionResult = this.backend
+      .saveQuery(this.query, this.title, this.comment, false)
+      .subscribe(() => {});
+  }
 }
