e7ecf4caf6a52fed531de9514241c2024cbefbe0
shah1375 <shayanmohajerani@gmail.com>
2023-10-18 16:52:04 +0200

    added function for enabling save button

--- src/app/modules/querybuilder/components/querybuilder-editor/save/save-dialog/resultant-query/resultant-query.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-editor/save/save-dialog/resultant-query/resultant-query.component.ts
@@ -1,33 +1,43 @@
-import { Component, OnDestroy } from '@angular/core';
-import { MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject, Input, OnDestroy, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Subscription } from 'rxjs';
 import { Query } from 'src/app/modules/querybuilder/model/api/query/query';
 import { BackendService } from 'src/app/modules/querybuilder/service/backend.service';
-import { SaveDialogComponent } from '../save-dialog.component';
+import { SaveDialogComponent, SaveDialogComponentData } from '../save-dialog.component';
 
 @Component({
   selector: 'num-resultant-query',
   templateUrl: './resultant-query.component.html',
   styleUrls: ['./resultant-query.component.scss'],
 })
-export class ResultantQueryComponent implements OnDestroy {
+export class ResultantQueryComponent implements OnInit, OnDestroy {
   title = '';
 
   comment = '';
 
   query: Query;
 
+  @Input()
+  letQuerySave: boolean;
+
   saveButtonDisabled = true;
 
-  constructor(
-    public backend: BackendService,
-    private dialogRef: MatDialogRef<SaveDialogComponent, void>
-  ) {}
+  constructor(public backend: BackendService) {}
 
   private subscriptionResult: Subscription;
 
+  ngOnInit() {
+    this.querySaveComparison();
+  }
+
   ngOnDestroy(): void {
     this.subscriptionResult?.unsubscribe();
+  }
+
+  querySaveComparison() {
+    if (this.letQuerySave && this.title.length > 0) {
+      this.saveButtonDisabled = false;
+    }
   }
 
   doSaveQuery(): void {
@@ -35,13 +45,5 @@ export class ResultantQueryComponent implements OnDestroy {
     this.subscriptionResult = this.backend
       .saveQuery(this.query, this.title, this.comment, true)
       .subscribe((response) => {});
-  }
-
-  isEmpty(): void {
-    this.saveButtonDisabled = this.title !== '';
-  }
-
-  doDiscard(): void {
-    this.dialogRef.close();
   }
 }
