aa7dc1474b98316f13a99b443fdb84e42f2dbf0d
shah1375 <shayanmohajerani@gmail.com>
2023-10-18 19:31:12 +0200

    clean up and size of dialog window for saving queries

--- src/app/modules/querybuilder/components/querybuilder-editor/save/save-dialog/save-dialog.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-editor/save/save-dialog/save-dialog.component.ts
@@ -1,19 +1,10 @@
 /* eslint-disable */
-import {
-  AfterViewChecked,
-  AfterViewInit,
-  Component,
-  Inject,
-  OnDestroy,
-  OnInit,
-} from '@angular/core'
+import { AfterViewChecked, Component, Inject, OnDestroy, OnInit } from '@angular/core'
 import { Query } from '../../../../model/api/query/query'
 import { QueryProviderService } from '../../../../service/query-provider.service'
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog'
-import { Router } from '@angular/router'
 import { BackendService } from '../../../../service/backend.service'
 import { Subscription } from 'rxjs'
-import { MatRadioChange } from '@angular/material/radio'
 import { FileSaverService } from 'ngx-filesaver'
 import { ApiTranslator } from '../../../../controller/ApiTranslator'
 
@@ -34,12 +25,11 @@ export class SaveDialogComponent implements OnInit, OnDestroy, AfterViewChecked 
   constructor(
     public queryProviderService: QueryProviderService,
     public backend: BackendService,
-    private fileSaverService: FileSaverService,
     @Inject(MAT_DIALOG_DATA) public data: SaveDialogComponentData,
-    private dialogRef: MatDialogRef<SaveDialogComponent, void>,
-    private apiTranslator: ApiTranslator
+    private dialogRef: MatDialogRef<SaveDialogComponent, void>
   ) {
     this.hasQuerySend = data.hasQuerySend
+    this.dialogRef.updateSize('50%', '45%')
   }
 
   query: Query
