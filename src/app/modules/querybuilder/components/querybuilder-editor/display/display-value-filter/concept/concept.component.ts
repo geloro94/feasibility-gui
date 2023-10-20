ccafd35a71acef1de6860ce33faa80311eea3465
shah1375 <shayanmohajerani@gmail.com>
2023-10-21 00:18:13 +0200

    input for attributes of type concept

--- src/app/modules/querybuilder/components/querybuilder-editor/display/display-value-filter/concept/concept.component.ts
+++ src/app/modules/querybuilder/components/querybuilder-editor/display/display-value-filter/concept/concept.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ValueFilter } from 'src/app/modules/querybuilder/model/api/query/valueFilter';
+import { TerminologyCode } from 'src/app/modules/querybuilder/model/api/terminology/terminology';
 
 @Component({
   selector: 'num-concept',
   templateUrl: './concept.component.html',
   styleUrls: ['./concept.component.scss'],
 })
-export class ConceptComponent {}
+export class ConceptComponent implements OnInit {
+  @Input()
+  attributeDefinitionConcepts: Array<TerminologyCode>;
+
+  ngOnInit() {}
+}
