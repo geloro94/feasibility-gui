afd4b8869555d1b77f29447fcd1d785900197440
shah1375 <shayanmohajerani@gmail.com>
2023-11-15 02:39:38 +0100

    clean up

--- src/app/model/FeasibilityQuery/Criterion/Criterion.ts
+++ src/app/model/FeasibilityQuery/Criterion/Criterion.ts
@@ -1,25 +1,25 @@
-import { TerminologyCode, TerminologyEntry } from '../../terminology/Terminology';
-import { ValueFilter } from './AttributeFilter/ValueFilter';
-import { TimeRestriction } from '../TimeRestriction';
 import { AttributeFilter } from './AttributeFilter/AttributeFilter';
 import { CritGroupPosition } from '../../../modules/querybuilder/controller/CritGroupArranger';
+import { TerminologyCode, TerminologyEntry } from '../../terminology/Terminology';
+import { TimeRestriction } from '../TimeRestriction';
+import { ValueFilter } from './AttributeFilter/ValueFilter';
 
 // A Criterion is an atomic building block of a query. However, a Criterion itself is defined by
 // a terminology code (system + version + code), operators and values.
 export class Criterion {
-  termCodes?: Array<TerminologyCode> = [];
-  display?: string;
-  entity?: boolean = false;
-  optional?: boolean = false;
-  valueFilters?: Array<ValueFilter> = [];
   attributeFilters?: Array<AttributeFilter> = [];
   children?: Array<TerminologyEntry> = [];
-  isinvalid?: boolean = false;
-  position?: CritGroupPosition;
-  linkedCriteria?: Criterion[] = [];
-  isLinked?: boolean = false;
-  context?: TerminologyCode;
   criterionHash?: string;
+  context?: TerminologyCode;
+  display?: string;
+  entity?: boolean = false;
+  isinvalid?: boolean = false;
+  isLinked?: boolean = false;
+  linkedCriteria?: Criterion[] = [];
+  optional?: boolean = false;
+  position?: CritGroupPosition;
   uniqueID?: string;
+  termCodes?: Array<TerminologyCode> = [];
   timeRestriction?: TimeRestriction;
+  valueFilters?: Array<ValueFilter> = [];
 }
