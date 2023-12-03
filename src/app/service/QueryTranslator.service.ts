b297987fcc094f5bed79ba62e6ceced88d463554
shah1375 <shayanmohajerani@gmail.com>
2023-12-03 13:01:50 +0100

    clean up

--- src/app/service/QueryTranslator.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { FeatureService } from './feature.service';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class QueryTranslatorService {
-  constructor(public featureService: FeatureService) {}
-}
