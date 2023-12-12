879310e07cf7a850ee6f82884eb1536d402951fc
shah1375 <shayanmohajerani@gmail.com>
2023-12-12 10:05:54 +0100

    clean up

--- src/app/model/StructuredQuery/Criterion/AttributeFilters/QueryFilters/QuantityFilter/AbstractQuantityFilter.ts
+++ src/app/model/StructuredQuery/Criterion/AttributeFilters/QueryFilters/QuantityFilter/AbstractQuantityFilter.ts
@@ -1,6 +1,7 @@
 import { FilterTypes } from 'src/app/model/FilterTypes';
 import { QuantityUnit } from 'src/app/model/FeasibilityQuery/Criterion/AttributeFilter/AbstractAttributeFilters';
 import { AbstractStructuredQueryFilters } from '../AbstractStructuredQueryFilters';
+import { TerminologyCode } from 'src/app/modules/querybuilder/model/api/terminology/terminology';
 
 export abstract class AbstractQuantityFilter extends AbstractStructuredQueryFilters {
   unit: QuantityUnit = new QuantityUnit();
