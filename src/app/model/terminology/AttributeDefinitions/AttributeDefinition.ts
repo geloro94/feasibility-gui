4d8d713b72cdc66beb9a1e23f1486e24924605c8
shah1375 <shayanmohajerani@gmail.com>
2024-03-11 11:09:05 +0100

    new variables for setting single reference termcodes and context and boolean

--- src/app/model/terminology/AttributeDefinitions/AttributeDefinition.ts
+++ src/app/model/terminology/AttributeDefinitions/AttributeDefinition.ts
@@ -1,8 +1,10 @@
-import { TerminologyCode } from '../Terminology';
+import { TerminologyCode, TerminologyEntry } from '../Terminology';
 import { AbstractAttributeDefinitions } from './AbstractAttributeDefinitions';
 
 export class AttributeDefinition extends AbstractAttributeDefinitions {
   attributeCode: TerminologyCode;
   referenceCriteriaSet: string;
+  referenceOnlyOnce = false;
+  singleReference?: TerminologyEntry;
 }
 export class ValueDefinition extends AbstractAttributeDefinitions {}
