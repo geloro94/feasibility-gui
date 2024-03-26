11f081090fffba7892c79c9088cc4935e673b37a
shah1375 <shayanmohajerani@gmail.com>
2024-03-26 15:46:27 +0100

    spelling fix

--- src/app/model/terminology/AttributeDefinitions/AttributeDefinition.ts
+++ src/app/model/terminology/AttributeDefinitions/AttributeDefinition.ts
@@ -4,7 +4,7 @@ import { AbstractAttributeDefinitions } from './AbstractAttributeDefinitions';
 export class AttributeDefinition extends AbstractAttributeDefinitions {
   attributeCode: TerminologyCode;
   referenceCriteriaSet: string;
-  referenceOnlyOnce = false;
+  referencedOnlyOnce = false;
   singleReference?: TerminologyEntry;
 }
 export class ValueDefinition extends AbstractAttributeDefinitions {}
